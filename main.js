872d688f71658a47bc8e9239c16c13fbbe54c2ce
ThunDerBoltzCodingClass <76979208+ThunDerBoltzCodingClass@users.noreply.github.com>
2023-05-18 20:48:51 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+video="";
+objects=[];
+
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(480, 380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(480, 380);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+        document.getElementById("status").innerHTML="Status: Objects Detected";
+        
+
+    for(i=0; i<objects.length; i++){
+        fill("blue");
+        percent=floor(objects[i].confidence * 100);
+        text(objects[i].label+" "+percent+"%", objects[i].x, objects[i].y);
+        noFill();
+        stroke("blue");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        if(objects[i].label == object_name){
+            video.stop();
+            objectDetector.detect(gotResult);
+            document.getElementById("object_status").innerHTML= object_name+" Found";
+            synth=window.speechSynthesis;
+            utterThis=new SpeechSynthesisUtterance(object_name+"Found");
+            synth.speak(utterThis);
+        }
+
+        else{
+            document.getElementById("object_status").innerHTML=object_name+" Not Found"
+        }
+
+        }
+    }
+}
+
+function start(){
+    objectDetector= ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+    object_name=document.getElementById("object_name").value;
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+
+}
